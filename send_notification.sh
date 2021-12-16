0a603c32c2918f1dbaeaba9cfa84e7b90b32eb73
baugistoms <toms.baugis@testdevlab.com>
2021-12-16 12:27:58 +0200

    new jenkinsfile and discord webhook

--- send_notification.sh
+++ send_notification.sh
@@ -14,7 +14,7 @@ fi
 
 # Send notification
 curl -X POST \
-  https://discordapp.com/api/webhooks/651765119101042689/JhoCn787dYW2s8c9CkqdYYizHTgUx9VT5pX65iCMfwYnC63C3Lc3tDGJsq0pnem3CD7B \
+  https://discord.com/api/webhooks/920983756234436649/WQSHYcJLwwf6POR-Ha6hvRfbWz_eHhh6UhHvXqVNyWsiwiF4fcbACtiuPIY1qMECEjRR \
   -H 'Cache-Control: no-cache' \
   -H 'Content-Type: application/json' \
   -d '{
